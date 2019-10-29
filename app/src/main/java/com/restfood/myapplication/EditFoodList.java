02770bd4f750d736413c97a86d3491a4e96b3423
BDomiyan <domiyan112@gmail.com>
2019-10-29 23:42:15 +0530

    update firestore data to recyclerview issue fixed

--- app/src/main/java/com/restfood/myapplication/EditFoodList.java
+++ app/src/main/java/com/restfood/myapplication/EditFoodList.java
@@ -6,29 +6,36 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
+
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.Collections;
+import java.util.List;
+
 
 public class EditFoodList extends AppCompatActivity {
     private RecyclerView rView;
     private RecyclerView.Adapter rAdapter;
     private RecyclerView.LayoutManager rLayoutManager;
 
-    public ArrayList<FoodData> foodList=new ArrayList<>();
+    ArrayList<FoodData> foodList=new ArrayList<>();
     private String uId;
     FoodData dat;
 
+    String demo="this is not good";
+
 
     private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private CollectionReference foodColRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +44,22 @@ public class EditFoodList extends AppCompatActivity {
 
 
 
-        foodList.add(new FoodData("Cheese Pizza","Pizza",250,10,20));
-        foodList.add(new FoodData("Sausage Pizza","Pizza",500,2,30));
+        this.foodList.add(new FoodData("Cheese Pizza","Pizza",250,10,20));
+        this.foodList.add(new FoodData("Sausage Pizza","Pizza",500,2,30));
+
 
         onbegi();
+
+        //this is pre process of ui or recyclerview
+        rView = findViewById(R.id.edit_food_list_recycler_view);
+        rView.setHasFixedSize(true);
+        rLayoutManager=new LinearLayoutManager(this);
+
+        postsetUi();
+
         getFoodList();
 
 
-
-
-
-        rView = findViewById(R.id.edit_food_list_recycler_view);
-        rView.setHasFixedSize(true);
-
-        rLayoutManager=new LinearLayoutManager(this);
-
-        //assigning list to adepter
-        rAdapter=new FoodListEditAdapter(foodList);
-
-        rView.setLayoutManager(rLayoutManager);
-        rView.setAdapter(rAdapter);
 
     }
 
@@ -66,9 +69,22 @@ public class EditFoodList extends AppCompatActivity {
         uId=auth.getUId();
     }
 
+    //used inside getFoodLsit
+    private void postsetUi()
+    {
+        //assigning list to adepter
+        rAdapter=new FoodListEditAdapter(foodList);
+
+        rView.setLayoutManager(rLayoutManager);
+        rView.setAdapter(rAdapter);
+
+
+    }
+
     //this function is to get food list form firestore
     private void getFoodList()
     {
+
         db.collection("shop")
                 .document(uId)
                 .collection("FoodList")
@@ -76,31 +92,40 @@ public class EditFoodList extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
-
                             if (task.isSuccessful()) {
+                                //this is temp list to get from database
+                                List<FoodData> list = new ArrayList<>();
                                 for (QueryDocumentSnapshot document : task.getResult()) {
-                                    Log.d("this_id", document.getId() + " => " + document.getData());
-                                    dat=document.toObject(FoodData.class);
+                                    //Log.d("this_id", document.getId() + " => " + document.getData());
 
-                                    Log.d("test this", dat.getFoodName());
-                                    Log.d("test this", dat.getCategory());
+                                    FoodData taskItem = document.toObject(FoodData.class);
+                                    list.add(taskItem);
 
-//                                    foodList.add(dat);
                                     foodList.add(new FoodData("Sausage Pizza","Pizza",500,2,30));
+//                                    int num=foodList.size();
+//                                    Log.d("number",String.valueOf(num));
+
+
 
 
                                 }
+//                                Toast.makeText(getApplicationContext(),demo,Toast.LENGTH_LONG).show();
+//                                Log.d("firestorelist", list.toString());
+                                Collections.copy(foodList,list);
+                                postsetUi();
+
                             } else {
+                                postsetUi();
+                                Toast.makeText(getApplicationContext(),"No Foods",Toast.LENGTH_LONG).show();
                 //                            Log.d(TAG, "Error getting documents: ", task.getException());
 
                             }
                         }
                     });
 
-        int num=foodList.size();
-        foodList.add(new FoodData("Sausage Pizza","Pizza",500,2,30));
-        Log.d("number",String.valueOf(num));
-        Toast.makeText(getApplicationContext(),String.valueOf(num),Toast.LENGTH_LONG).show();
+
+
+
 
 
 
