c9fbc5d6740bd72f8975fc5a3383238a91b4f550
OsandaLelum <41205668+OsandaLelum@users.noreply.github.com>
2020-02-12 20:28:11 +0530

    Merge branch 'master' of C:\Users\Osanda\Documents\GitHub\RestFood-Shop with conflicts.

---
+++ app/src/main/java/com/restfood/myapplication/Inventry.java
@@ -0,0 +1,38 @@
+package com.restfood.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Inventry extends AppCompatActivity {
+    FloatingActionButton buttonAdd;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventry);
+
+        buttonAdd=findViewById(R.id.floatingbutton_invent);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InventryBottomSheetDialog bottomsheet=new InventryBottomSheetDialog();
+                bottomsheet.show(getSupportFragmentManager(),"Hi this is ");
+            }
+        });
+    }
+
+    private void add()
+    {
+        InventryBottomSheetDialog bottomsheet=new InventryBottomSheetDialog();
+        bottomsheet.show(getSupportFragmentManager(),"Hi this is ");
+    }
+}
---
+++ app/src/main/java/com/restfood/myapplication/InventryData.java
@@ -0,0 +1,23 @@
+package com.restfood.myapplication;
+
+public class InventryData {
+    String name;
+    float quantity;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+}
